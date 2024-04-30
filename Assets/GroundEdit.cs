5564376b0ecd62efed50c40f2d2d67d961c914f2
Elena Scobici <elena@scobici.com>
2024-04-30 16:15:20 -0400

    Allow player to click and drag soil or grass tiles

--- Assets/GroundEdit.cs
+++ Assets/GroundEdit.cs
@@ -9,13 +9,24 @@ public class GroundEdit : MonoBehaviour
     public RuleTile soilTile;
     public Tile grassTile;
     public Tilemap tileMap;
+    private bool soilMode = true;
     void Update() {
+        // As soon as left button is clicked, record whether a
+        // grass or soil tile has been clicked to determine whether
+        // we are in soil tile mode or grass tile mode.
         if (Input.GetMouseButtonDown(0)) {
             Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var tpos = tileMap.WorldToCell(worldPoint);
             var tile = tileMap.GetTile(tpos);
+            soilMode = tile.name.Contains("Ground");
+        }
+        // While user holds and drags left click button, update all
+        // tiles based on mode.
+        if (Input.GetMouseButton(0)) {
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var tpos = tileMap.WorldToCell(worldPoint);
 
-            if (tile.name.Contains("Ground")) {
+            if (soilMode) {
                 tileMap.SetTile(tpos, soilTile);
             }
             else {
