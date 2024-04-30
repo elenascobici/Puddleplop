69e8293a605cf27c4323d1bbcc8923d9ebb3dd1e
Elena Scobici <elena@scobici.com>
2024-04-30 16:02:35 -0400

    Allow user to toggle between soil and grass tiles

--- Assets/GroundEdit.cs
+++ Assets/GroundEdit.cs
@@ -7,22 +7,20 @@ using UnityEngine.Tilemaps;
 public class GroundEdit : MonoBehaviour
 {
     public RuleTile soilTile;
+    public Tile grassTile;
     public Tilemap tileMap;
-    private Vector3 worldPoint;
     void Update() {
-        if (Input.GetMouseButtonDown(0))
-            {
-                worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    
-                var tpos = tileMap.WorldToCell(worldPoint);
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var tpos = tileMap.WorldToCell(worldPoint);
+            var tile = tileMap.GetTile(tpos);
 
-                // Try to get a tile from cell position
-                var tile = tileMap.GetTile(tpos);
-
-                if (tile)
-                {
-                    tileMap.SetTile(tpos, soilTile);
-                }
+            if (tile.name.Contains("Ground")) {
+                tileMap.SetTile(tpos, soilTile);
             }
+            else {
+                tileMap.SetTile(tpos, grassTile);
+            }
+        }
     }
 }
