215cfae42e8e8aba8af35dfb3388df2d7bc79ea4
Elena Scobici <elena@scobici.com>
2024-08-21 13:22:58 -0400

    Update ground edit to work with new user data format

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -13,6 +13,10 @@ public class Tile
 {
     public int x;
     public int y;
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 [Serializable]
--- Assets/Scripts/GroundEdit.cs
+++ Assets/Scripts/GroundEdit.cs
@@ -5,12 +5,12 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Newtonsoft.Json;
+using System.Numerics;
 public class GroundEdit : MonoBehaviour
 {
     // Reference to the ground editing toggle script so we can
     // check whether ground editing has been enabled.
     public GroundEditButtonScript groundEditButtonScript;
-    public UserData userData;
     public GameObject frog;
     const int WIDTH = 14;
     const int HEIGHT = 8;
@@ -19,6 +19,8 @@ public class GroundEdit : MonoBehaviour
     public Tilemap tileMap;
     private bool soilMode = true;
     private TileBase[,] initTileArray = new TileBase[EDITABLE_LENGTH+1, HEIGHT];
+    private DataManager dataManager;
+    private UserData userData;
 
     // Define the corners of the area in which the player can place
     // soil.
@@ -26,6 +28,8 @@ public class GroundEdit : MonoBehaviour
     private Tuple<int,int,int> BOTTOM_RIGHT = Tuple.Create(13,-4,0);
 
     void Start() {
+        dataManager = DataManager.Instance;
+        userData = dataManager.GetUserData();
         // Record the initial grass tiles, so that we can replace
         // soil tiles with the grass tile that were there before.
         for (int i = 0; i <= EDITABLE_LENGTH; i++) {
@@ -33,18 +37,10 @@ public class GroundEdit : MonoBehaviour
                 initTileArray[i, j] = tileMap.GetTile(new Vector3Int(i - WIDTH/2, j - HEIGHT/2, 0));
             }
         }
-        // string groundDataString = userData.data.ContainsKey("soilTiles")
-        //     ? userData.data["soilTiles"] : "\"\"";
-        // groundData = JsonConvert.DeserializeObject<GroundData>("{\"soilTiles\":" + groundDataString + "}");
-        // if (groundData.soilTiles == null) { // No soil tiles
-        //     groundData.soilTiles = new List<GroundCoords>();
-        // }
-        // else { // Otherwise, load the soil tiles visually
-        //     foreach (GroundCoords soilCoord in groundData.soilTiles) {
-        //         tileMap.SetTile(new Vector3Int(soilCoord.x, soilCoord.y,
-        //             soilCoord.z), soilTile);
-        //     }
-        // }
+
+        foreach(Tile tile in userData.tiles) {
+            tileMap.SetTile(new Vector3Int(tile.x, tile.y, 0), soilTile);
+        }
     }
 
 
@@ -53,7 +49,7 @@ public class GroundEdit : MonoBehaviour
         // grass or soil tile has been clicked to determine whether
         // we are in soil tile mode or grass tile mode.
         if (Input.GetMouseButtonDown(0) && groundEditButtonScript.groundEditEnabled) {
-            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            UnityEngine.Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var tpos = tileMap.WorldToCell(worldPoint);
             var tile = tileMap.GetTile(tpos);
             soilMode = tile.name.Contains("Ground");
@@ -61,23 +57,24 @@ public class GroundEdit : MonoBehaviour
         // While user holds and drags left click button, update all
         // tiles based on mode.
         if (Input.GetMouseButton(0) && groundEditButtonScript.groundEditEnabled) {
-            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            UnityEngine.Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var tpos = tileMap.WorldToCell(worldPoint);
 
             // Only edit the ground within the given bounds.
             if (InBounds(tpos)) {
                 if (soilMode) {
+                    if (tileMap.GetTile(tpos).name.Contains("Ground")) {
+                        userData.tiles.Add(new Tile(tpos.x, tpos.y));
+                    }
                     tileMap.SetTile(tpos, soilTile);
-                    // groundData.Add(tpos);
-                    // userData.SaveKeyAndValue("soilTiles",
-                    //     JsonConvert.SerializeObject(groundData.soilTiles));
+                    dataManager.SaveUserData();
                 }
                 else if (!tileMap.GetTile(tpos).name.Contains("Ground")) {
                     // Replace the soil tile with the initial grass tile.
                     tileMap.SetTile(tpos, initTileArray[tpos[0] + WIDTH/2, tpos[1] + HEIGHT/2]);
-                    // groundData.Remove(tpos);
-                    // userData.SaveKeyAndValue("soilTiles",
-                    //     JsonConvert.SerializeObject(groundData.soilTiles));
+                    userData.tiles.Remove(userData.tiles.FirstOrDefault(t => t.x == tpos.x && t.y == tpos.y));
+                    print("t.x: " + tpos.x);
+                    dataManager.SaveUserData();
                 }
             }
         }
